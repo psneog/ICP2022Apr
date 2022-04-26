a65b60ca0918b958320c8473b41899565ed9e4fa
Prabhakar Sarma Neog <psneog@gmail.com>
2022-04-26 09:24:16 +0530

    Class on 26th Apr 2022

---
+++ hello.c
@@ -0,0 +1,18 @@
+/******************************************************************
+Program to display the string "Hello world"
+******************************************************************/
+
+// Include header file
+
+# include <stdio.h>   		// Preprocessor directives starts with #
+
+// The main function. When run, it starts executing from here
+
+void main() {
+	printf("Hello world!\n");
+	return;
+}
+
+
+
+
---
+++ template.c
@@ -0,0 +1,12 @@
+/**********************************************************
+Program to
+***********************************************************/
+
+// Headers
+# include <stdio.h>
+
+// The main() function
+void main() {
+	printf("Hello World!\n");
+	return;
+}
